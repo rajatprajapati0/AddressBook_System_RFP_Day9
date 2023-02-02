4ec73596de60bd3c2498113d46fd42d5ed6b325a
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-02 14:47:08 +0530

    Added: program file wtith welcome statement

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Address_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book\r\nProgram in");
+            Console.ReadKey();
+        }
+    }
+}
