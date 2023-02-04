d05c00803ad45b736bbc23a10a641af01f645443
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-05 04:45:59 +0530

    Added:UC1 Program file with contact file

---
+++ Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book
+{
+    public class Contact
+    {
+        public string firstName;
+        public string lastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zipcode;
+        public string Phone;
+        public string Email;
+
+        public override string ToString()
+        {
+          return @$"
+          Name: {firstName} {lastName}
+          Address: {this.Address} 
+          City: {this.City}
+          State: {this.State}
+          Zipcode: {this.Zipcode}
+          PhoneNumber:{this.Phone}
+          Email: {this.Email}
+                   ";
+          }  
+    }
+}   
--- Program.cs
+++ Program.cs
@@ -2,12 +2,33 @@
 
 namespace Address_Book
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book\r\nProgram in");
+            Contact contact = new Contact();
+            Console.WriteLine("Enter your First Name");
+            contact.firstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name");
+            contact.lastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address ");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter your City name ");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter your State name");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter your zipcode");
+            contact.Zipcode = Console.ReadLine();
+            Console.WriteLine("Enter your Phone number");
+            contact.Phone = Console.ReadLine();
+            Console.WriteLine("Enter your email ");
+            contact.Email = Console.ReadLine();
+
+            string data=  contact.ToString();
+            Console.WriteLine(data);
             Console.ReadKey();
+
         }
     }
 }
